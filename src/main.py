07608e8975c2c0c93e95022b421629cbc5067a78
Olle Olsson <ohjolsson@gmail.com>
2013-09-25 19:42:02 +0200

    Playerclass

--- src/main.py
+++ src/main.py
@@ -13,7 +13,8 @@ WIDTH       = NUMCOLS * TILESIZE + 100
 HEIGHT      = NUMROWS * TILESIZE + 100
 YMARGIN     = (HEIGHT - TILESIZE * NUMROWS) / 2
 XMARGIN     = (WIDTH - TILESIZE * NUMCOLS) / 2 
-PLAYERSPEED = TILESIZE / 6
+PLAYERSPEED = 2
+PLAYERSIZE  = 10
 
 assert TILESIZE % PLAYERSPEED == 0, "Bad tilesize - playerspeed ratio"
 # assert YMARGIN + SECTORSIZE*NUMROWS == HEIGHT, "Height inconsistency"
@@ -25,6 +26,27 @@ RED     = (255, 0,   0)
 GRAY    = (100, 100, 100)
 GREEN   = (0,   255,   0)
 BLUE    = (0,   0,   255)
+
+class Player:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.movingLeft     = False
+        self.movingRight    = False
+        self.movingUp       = False
+        self.movingDown     = False
+
+    def setMovingLeft(self, move):
+        self.movingLeft = move
+
+    def setMovingRight(self, move):
+        self.movingRight = move
+
+    def setMovingUp(self, move):
+        self.movingUp = move
+
+    def setMovingDown(self, move):
+        self.movingDown = move
 
 def main():
     DISPLAYSURF = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -42,28 +64,46 @@ def main():
     [0, 1, 1, 1, 1, 0],
     [0, 0, 0, 0, 0, 0] ]
 
-    player1 = pygame.Rect(XMARGIN + TILESIZE, YMARGIN + TILESIZE, 5, 5)
+    player1 = Player(XMARGIN + TILESIZE, YMARGIN + TILESIZE)
 
     pygame.display.set_caption("Splinter block")
-
     while 1:
         DISPLAYSURF.fill(BLACK)
         anisurf.fill(BLACK)
 
         for event in pygame.event.get():
+            if event.type == KEYDOWN:
+                if event.key == K_LEFT :
+                    player1.setMovingLeft(True)
+                if event.key == K_RIGHT :
+                    player1.setMovingRight(True)       
+                if event.key == K_UP :
+                    player1.setMovingUp(True)                           
+                if event.key == K_DOWN :
+                    player1.setMovingDown(True)       
+
             if event.type == KEYUP:
                 if event.key == K_LEFT :
-                    player1.left -= PLAYERSPEED
+                    player1.setMovingLeft(False)
                 if event.key == K_RIGHT :
-                    player1.left += PLAYERSPEED
+                    player1.setMovingRight(False)       
                 if event.key == K_UP :
-                    player1.top -= PLAYERSPEED
+                    player1.setMovingUp(False)                           
                 if event.key == K_DOWN :
-                    player1.top += PLAYERSPEED
+                    player1.setMovingDown(False)       
 
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
+
+        if player1.movingLeft:
+            player1.x -= PLAYERSPEED
+        if player1.movingRight:
+            player1.x += PLAYERSPEED
+        if player1.movingUp:
+            player1.y -= PLAYERSPEED
+        if player1.movingDown:
+            player1.y += PLAYERSPEED
 
         for row in range(NUMROWS):
             for col in range(NUMCOLS):
@@ -75,11 +115,12 @@ def main():
                     pygame.draw.rect(anisurf, GREEN, tileRect)
                 
 
-        pygame.draw.rect(anisurf, BLUE, player1)
+        pygame.draw.rect(anisurf, BLUE, pygame.Rect(player1.x, player1.y, 
+            PLAYERSIZE, PLAYERSIZE))
 
         DISPLAYSURF.blit(anisurf, (0,0))
         pygame.display.update()
-        fpsClock.tick()
+        fpsClock.tick(FPS)
 
 def validPos(x, y, level):
     pass
